dbab64a2cb8963cbf0a743697adb1777cedd3651
Vincent <vincentmuriuki42@gmail.com>
2018-09-19 10:41:29 +0300

    Feature (Description) Add POST API endpoint
    This endpoint allows users to post new food orders
    [finishes #160611297]

---
+++ v1/app/orders.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_restful import    Resource
+from .models import CustomerOrder, food_orders
+
+class PostOrder(Resource):
+
+    def post(self):
+        data = request.get_json()
+        order = CustomerOrder(data['name'], data["price"],data['description'])
+        food_orders.append(order)
+
+        return jsonify({'Message' : 'Congratulations. Your new order has been posted. Kindly wait!'}), 201
